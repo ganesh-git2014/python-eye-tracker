a80ced12d5bff1c9b18569b47903e7267845ca03
rcbyron <rcbyron@utexas.edu>
2015-11-09 12:02:17 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # python-eye-tracker
+
+HAAR Cascade File Link:
+https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+Installation:
+- Build and Install OpenCV 3 for Python 3 using CMake
+- Run face_detect.py
---
+++ face_detect.py
@@ -0,0 +1,37 @@
+import cv2
+import sys
+
+FRAME_TITLE = "Python Eye Tracker 6000"
+CASCADE_PATH = "haarcascades/haarcascade_eye.xml"
+
+obj_cascade = cv2.CascadeClassifier(CASCADE_PATH)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    detected_objs = obj_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=2,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in detected_objs:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow(FRAME_TITLE, frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
